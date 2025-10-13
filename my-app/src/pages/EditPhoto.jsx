8cffb91f96fdce004e3f3da3e7c62f660980852c
Array <ArrayFounder@gmail.com>
2025-10-14 00:33:10 +0700

    Test

--- my-app/src/pages/EditPhoto.jsx
+++ my-app/src/pages/EditPhoto.jsx
@@ -6,7 +6,7 @@ import safeStorage from '../utils/safeStorage.js';
 import QRCode from 'qrcode';
 import { convertBlobToMp4 } from '../utils/videoTranscoder.js';
 import swapIcon from '../assets/swap.png';
-// FremioSeries Imports
+// FremioSeries Imports.
 import FremioSeriesBlue2 from '../assets/frames/FremioSeries/FremioSeries-2/FremioSeries-blue-2.png';
 import FremioSeriesBabyblue3 from '../assets/frames/FremioSeries/FremioSeries-3/FremioSeries-babyblue-3.png';
 import FremioSeriesBlack3 from '../assets/frames/FremioSeries/FremioSeries-3/FremioSeries-black-3.png';

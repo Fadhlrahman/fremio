0481ccf95acb2c0873194c8a7958fc0a95dff4a3
Array <ArrayFounder@gmail.com>
2025-12-01 14:29:18 +0700

    v13.1: Add version marker, prevent cache

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+// App version for debugging cache issues
+console.log('🚀 Fremio App v13 - Build: 2025-12-01T07:30');
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter
